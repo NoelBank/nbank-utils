d00bd6e1c619c01bbf5dadd76227dd473c001dad
Noel Bank <info@nbank.codes>
2020-05-18 17:40:17 +0200

    feat: add on Player join Player to DB

--- src/main/java/codes/nbank/plugins/utils/listener/JoinMessage.java
+++ src/main/java/codes/nbank/plugins/utils/listener/JoinMessage.java
@@ -1,14 +1,34 @@
 package codes.nbank.plugins.utils.listener;
 
+import codes.nbank.plugins.utils.Database;
 import codes.nbank.plugins.utils.Mods.handler.TabList;
+import codes.nbank.plugins.utils.Utils;
+import com.mongodb.client.MongoCollection;
+import org.bson.Document;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 
 public class JoinMessage implements Listener {
+    MongoCollection playerCollection = null;
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent event) {
         new TabList().setScoreboard(event.getPlayer());
-        event.setJoinMessage("§2» §7" + event.getPlayer().getDisplayName());
+        Player player = event.getPlayer();
+        event.setJoinMessage("§2» §7" + player.getDisplayName());
+        String playerUUID = player.getUniqueId().toString();
+
+
+        playerCollection = Database.mongoDatabase.getCollection(playerUUID);
+
+        if(playerCollection == null) {
+            Database.mongoDatabase.createCollection(playerUUID);
+            playerCollection = Database.mongoDatabase.getCollection(playerUUID);
+        }
+
+        Document playerInfo = new Document("uuid", event.getPlayer().getUniqueId().toString()).append("name", player.getName());
+
+        playerCollection.insertOne(playerInfo);
     }
 }
